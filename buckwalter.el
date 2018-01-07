2aa5451c3682c268adebc6b1191a796466732f53
Joe H. Rahme <jhakimra@redhat.com>
2018-01-07 17:43:02 +0100

    Moves the license to GPLv3 to satisfy MELPA
    For all the "freedom" they champion, the GPL people sure have a lot of
    opinion on my code's licensing...

--- buckwalter.el
+++ buckwalter.el
@@ -20,7 +20,7 @@
 ;; This file is NOT part of GNU Emacs.
 
 ;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License v2 as
+;; modify it under the terms of the GNU General Public License v3 as
 ;; published by the Free Software Foundation.
 
 ;; This program is distributed in the hope that it will be useful,
